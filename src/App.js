d421a11aa9803e8c10e510230d35f1f123b5e21e
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-06-05 15:09:58 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import './App.css';
 import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="meals">
-      <span className="logo">TRYBE</span>
+      <span className="logo">TRYBE !</span>
       <object
         className="rocksGlass"
         type="image/svg+xml"
