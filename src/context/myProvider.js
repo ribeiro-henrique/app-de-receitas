b1e96b476834880b14eed235053adc4c74e247d3
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-06-07 20:07:55 -0300

    resolvido propType

--- src/context/myProvider.js
+++ src/context/myProvider.js
@@ -1,27 +1,24 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import context from './myContext';
 
 function MyProvider({ children }) {
   const [foods, setFoods] = useState([]);
 
-  const contextValue = {
+  const contextValue = useMemo(() => ({
     foods,
     setFoods,
-  };
+  }), [foods, setFoods]);
 
   return (
     <context.Provider value={ contextValue }>
-      { children }
+      {children}
     </context.Provider>
   );
 }
 
 MyProvider.propTypes = {
   children: PropTypes.node.isRequired,
-  foods: PropTypes.array.isRequired,
-  setFoods: PropTypes.func.isRequired,
 };
 
 export default MyProvider;
